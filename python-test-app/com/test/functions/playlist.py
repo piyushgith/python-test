d9bc13bbfc2fba8e3c43de106440836953f42e1c
piyushprasad2007 <piyushprasad2007@gmail.com>
2022-12-25 19:25:28 +0530

    working playlist or video

--- python-test-app/com/test/functions/playlist.py
+++ python-test-app/com/test/functions/playlist.py
@@ -1,11 +1,11 @@
 from pytube import YouTube, Playlist
 import random
 import time
+import gc
 
 target_dir = 'D:/YouTube'
 #playlist = Playlist('https://www.youtube.com/watch?v=PN7YFKHOR9Y&list=PL7RtZMiaOk8gdRf130w4gFYyhstL-5VRh')
-urlList=[]
-playListDict={}
+
 
 def GetPlayListUrls(youTubeLink):
     substring = "&list"
@@ -18,9 +18,12 @@ def GetPlayListUrls(youTubeLink):
 
         for video in playlist.videos:
             print('downloading : {} with url : {}'.format(video.title, video.watch_url))
-            time.sleep(random.randint(60,100))
-            Download(video)
+            Download(video.watch_url)
             print('Downloaded Video : {}'.format(video.title))
+            sleep_time = random.randint(30,100)
+            print('Sleep for next Video : {} Seconds'.format(sleep_time))
+            #Need to sleep to Bypass Youtube Download Limit from Single IP
+            time.sleep(sleep_time)
             #print('"{}":"{}",'.format(video.title, video.watch_url))
     else:
         print("This is single video found!")
@@ -33,28 +36,16 @@ def Download(link):
         print("Starting download....")
         youtubeObject.streams.filter(type='video', progressive=True, file_extension='mp4'). \
             order_by('resolution').desc().first().download(target_dir)
+        #GC is needed if we download a playlist
+        youtubeObject = None
+        gc.collect()
     except:
         print("An error has occurred")
     print("Download is completed successfully!!!!!!")
 
 
+
 #Calling methods after running the file
 playListLink = input("Enter the YouTube Playlist URL: ")
 GetPlayListUrls(playListLink)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
